680ee0056ae5a7d7923825da6bbe0a443f25cfd4
Sandile Shongwe <yakka.sandile@gmail.com>
2021-04-05 14:59:44 +0200

    Updates provider

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # aws-terraform-static-web-hosting
+
+## Quick Bootstrapper to help you deploy s3 bucket for static webhosting and create Route53 record to point to the bucket
--- provider.tf
+++ provider.tf
@@ -1,7 +1,3 @@
 provider "aws" {
     region           = "af-south-1"
-    assume_role {
-        session_name = "terraform-operator"
-        role_arn     = "arn:aws:iam::516599252282:role/terraform-operator"
-    }
 }
